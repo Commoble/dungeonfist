da3f4ddce7414f72e1c06eac2a957ea934fedc1e
Commoble <josephbettendorff@yahoo.com>
2019-11-15 17:21:26 -0600

    Added more loot tables, added means of autogenerating loot tables via runData

---
+++ src/main/java/com/github/commoble/dungeonfist/data/loot/DungeonChestLootTables.java
@@ -0,0 +1,82 @@
+package com.github.commoble.dungeonfist.data.loot;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+import com.github.commoble.dungeonfist.DungeonFist;
+
+import net.minecraft.item.Items;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.storage.loot.ConstantRange;
+import net.minecraft.world.storage.loot.ItemLootEntry;
+import net.minecraft.world.storage.loot.LootParameterSets;
+import net.minecraft.world.storage.loot.LootPool;
+import net.minecraft.world.storage.loot.LootTable;
+import net.minecraft.world.storage.loot.LootTable.Builder;
+import net.minecraft.world.storage.loot.RandomValueRange;
+import net.minecraft.world.storage.loot.TableLootEntry;
+import net.minecraft.world.storage.loot.functions.EnchantWithLevels;
+
+public class DungeonChestLootTables implements Consumer<BiConsumer<ResourceLocation, LootTable.Builder>>
+{
+	public ResourceLocation TEST_CHEST = new ResourceLocation(DungeonFist.MODID, "chests/test");
+	
+	// subtable locations
+	public ResourceLocation ENCHANTED_WEAPONS = new ResourceLocation(DungeonFist.MODID, "subtables/enchanted_weapons");
+	public ResourceLocation WEAPONS = new ResourceLocation(DungeonFist.MODID, "subtables/weapons");
+	
+	public ResourceLocation WOODEN_TOOLS_TAG = new ResourceLocation(DungeonFist.MODID, "wooden_tools");
+	
+	@Override
+	public void accept(BiConsumer<ResourceLocation, Builder> builderConsumer)
+	{
+		builderConsumer.accept(
+			this.WEAPONS,
+			LootTable.builder().addLootPool(
+				LootPool.builder().rolls(ConstantRange.of(1))
+				.addEntry(ItemLootEntry.builder(Items.IRON_SWORD).weight(40))
+				.addEntry(ItemLootEntry.builder(Items.IRON_AXE).weight(40))
+				.addEntry(ItemLootEntry.builder(Items.BOW).weight(11))
+				.addEntry(ItemLootEntry.builder(Items.GOLDEN_SWORD).weight(1))
+				.addEntry(ItemLootEntry.builder(Items.GOLDEN_AXE).weight(1))
+				.addEntry(ItemLootEntry.builder(Items.WOODEN_SWORD).weight(40))
+				.addEntry(ItemLootEntry.builder(Items.WOODEN_AXE).weight(1))
+				.addEntry(ItemLootEntry.builder(Items.DIAMOND_SWORD).weight(1))
+				.addEntry(ItemLootEntry.builder(Items.DIAMOND_AXE).weight(1))
+				.addEntry(ItemLootEntry.builder(Items.TRIDENT).weight(1))
+			)
+		);
+		
+		builderConsumer.accept(
+			this.ENCHANTED_WEAPONS,
+			LootTable.builder().addLootPool(
+				LootPool.builder().rolls(ConstantRange.of(1)).addEntry(
+					TableLootEntry.builder(this.WEAPONS).weight(1).acceptFunction(
+						// unmapped function names: This gets the builder for EnchantWithLevels,
+						// sets the random level range to between 1 and 5, and enables treasure enchantments
+						EnchantWithLevels.func_215895_a(RandomValueRange.func_215837_a(1F, 5F)).func_216059_e()
+					)
+				).addEntry(
+					TableLootEntry.builder(this.WEAPONS).weight(1).acceptFunction(
+						EnchantWithLevels.func_215895_a(RandomValueRange.func_215837_a(10F, 20F)).func_216059_e()
+					)
+				)
+			)
+		);
+		
+		builderConsumer.accept(
+			this.TEST_CHEST,
+			LootTable.builder().setParameterSet(LootParameterSets.CHEST).addLootPool(
+				LootPool.builder().rolls(RandomValueRange.func_215837_a(3F, 10F)).addEntry(
+					TableLootEntry.builder(this.ENCHANTED_WEAPONS).acceptFunction(
+						ApplyFunctionsIfItemHasTag.getBuilder(
+							this.WOODEN_TOOLS_TAG, 
+							ApplyFunctionsIfItemHasTag.getSetNameBuilder(new StringTextComponent("Yep, it's wood"), null).build()
+						)
+					)
+				)
+			)
+		);
+	}
+}
---
+++ src/main/java/com/github/commoble/dungeonfist/data/loot/DungeonLootTableProvider.java
@@ -0,0 +1,59 @@
+package com.github.commoble.dungeonfist.data.loot;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import com.github.commoble.dungeonfist.DungeonFist;
+import com.google.common.collect.ImmutableList;
+import com.mojang.datafixers.util.Pair;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.LootTableProvider;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.loot.LootParameterSet;
+import net.minecraft.world.storage.loot.LootParameterSets;
+import net.minecraft.world.storage.loot.LootTable;
+import net.minecraft.world.storage.loot.LootTableManager;
+import net.minecraft.world.storage.loot.ValidationResults;
+
+public class DungeonLootTableProvider extends LootTableProvider
+{
+	// if you're familiar with the top-level property "type" in loot tables
+	// e.g. "type": "minecraft:chest" or "type": "minecraft:generic"
+	// this determines what sorts of parameters are available to the loot table for conditions, etc
+	// and when we generate loot tables, we define them with their parameter sets in this list here
+	// technically, loot table builders have a method to set it in the builder
+	// but this seems to be ALWAYS overridden but what we define here
+	private final List<Pair<Supplier<Consumer<BiConsumer<ResourceLocation, LootTable.Builder>>>, LootParameterSet>> loot_tables =
+			ImmutableList.of(Pair.of(DungeonChestLootTables::new, LootParameterSets.CHEST));
+	
+	public DungeonLootTableProvider(DataGenerator dataGenerator)
+	{
+		super(dataGenerator);
+	}
+
+	@Override
+	protected List<Pair<Supplier<Consumer<BiConsumer<ResourceLocation, LootTable.Builder>>>, LootParameterSet>> getTables()
+	{
+		return this.loot_tables;
+	}
+
+	@Override
+	public String getName()
+	{
+		// TODO Auto-generated method stub
+		return DungeonFist.MODID + ":loot_tables";
+	}
+
+	@Override
+	protected void validate(Map<ResourceLocation, LootTable> map, ValidationResults validationresults)
+	{
+		map.forEach((resourceLocation, lootTable) -> {
+			LootTableManager.func_215302_a(validationresults, resourceLocation, lootTable, map::get);
+		});
+	}
+
+}
