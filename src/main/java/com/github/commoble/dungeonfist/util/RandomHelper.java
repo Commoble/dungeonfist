039e631c5085cd5b50d1d467b4ad1aa535012f80
Commoble <josephbettendorff@yahoo.com>
2019-11-18 21:30:38 -0600

    Added words for epic armor names

--- src/main/java/com/github/commoble/dungeonfist/util/RandomHelper.java
+++ src/main/java/com/github/commoble/dungeonfist/util/RandomHelper.java
@@ -1,6 +1,7 @@
 package com.github.commoble.dungeonfist.util;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 public class RandomHelper
@@ -14,5 +15,28 @@ public class RandomHelper
 	public static <T> T chooseRandomThing(Random rand, T...things)
 	{
 		return things[rand.nextInt(things.length)];
+	}
+	
+	/** Returns a random thing from a list of lists of things, weighted appropriate as if it were a single list of things,
+	 * returns empty optional if all lists are empty 
+	 * @param rand
+	 * @param lists
+	 * @return
+	 */
+	public static <T> Optional<T> getRandomThingFromMultipleLists(Random rand, List<? extends List<T>> lists)
+	{
+		// get sum of sizes of lists
+		int totalSize = lists.stream().reduce(0, (size, list) -> size + list.size(), (sizeA,sizeB) -> sizeA+sizeB);
+		int index = rand.nextInt(totalSize);
+		for (List<T> list : lists)
+		{
+			int sizeOfThisList = list.size();
+			if (index < sizeOfThisList)
+			{
+				return Optional.of(list.get(index));
+			}
+			index = index - sizeOfThisList;
+		}
+		return Optional.empty();
 	}
 }
