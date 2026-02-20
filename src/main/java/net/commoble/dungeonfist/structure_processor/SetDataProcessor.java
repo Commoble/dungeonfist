242a1c4a0c08e0dd01a7926cb41e64204d3a0614
Commoble <josephbettendorff@yahoo.com>
2026-02-19 19:40:08 -0500

    Replace glow lichen with vines, add chance of spawners to some secret rooms

--- src/main/java/net/commoble/dungeonfist/structure_processor/SetDataProcessor.java
+++ src/main/java/net/commoble/dungeonfist/structure_processor/SetDataProcessor.java
@@ -2,6 +2,7 @@ package net.commoble.dungeonfist.structure_processor;
 
 import org.jspecify.annotations.Nullable;
 
+import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
@@ -10,6 +11,7 @@ import net.commoble.dungeonfist.DungeonFist;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderSet;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -23,16 +25,19 @@ public class SetDataProcessor extends StructureProcessor
 {
 	public static final MapCodec<SetDataProcessor> CODEC = RecordCodecBuilder.mapCodec(builder -> builder.group(
 			CodecPuncher.BLOCKS.fieldOf("blocks").forGetter(SetDataProcessor::blocks),
-			CompoundTag.CODEC.fieldOf("data").forGetter(SetDataProcessor::data)
+			CompoundTag.CODEC.fieldOf("data").forGetter(SetDataProcessor::data),
+			Codec.FLOAT.optionalFieldOf("probability", 1F).forGetter(SetDataProcessor::probability)
 		).apply(builder, SetDataProcessor::new));
 	
 	private final HolderSet<Block> blocks;
 	private final CompoundTag data;
+	private final float probability;
 	
-	public SetDataProcessor(HolderSet<Block> blocks, CompoundTag data)
+	public SetDataProcessor(HolderSet<Block> blocks, CompoundTag data, float probability)
 	{
 		this.blocks = blocks;
 		this.data = data;
+		this.probability = probability;
 	}
 	
 	public HolderSet<Block> blocks()
@@ -45,6 +50,11 @@ public class SetDataProcessor extends StructureProcessor
 		return this.data;
 	}
 	
+	public float probability()
+	{
+		return this.probability;
+	}
+	
 	@Override
 	protected StructureProcessorType<?> getType()
 	{
@@ -55,15 +65,17 @@ public class SetDataProcessor extends StructureProcessor
 	public @Nullable StructureBlockInfo process(LevelReader level, BlockPos targetPosition, BlockPos referencePos, StructureBlockInfo originalBlockInfo,
 		StructureBlockInfo processedBlockInfo, StructurePlaceSettings settings, @Nullable StructureTemplate template)
 	{
+		BlockPos pos = processedBlockInfo.pos();
+        RandomSource random = settings.getRandom(pos);
 		BlockState state = processedBlockInfo.state();
-		if (state.is(this.blocks))
+		if (random.nextFloat() < this.probability && state.is(this.blocks))
 		{
 			@Nullable CompoundTag oldTag = processedBlockInfo.nbt();
 			CompoundTag baseTag = oldTag == null
 				? new CompoundTag()
 				: oldTag.copy();
 			return new StructureBlockInfo(
-				processedBlockInfo.pos(),
+				pos,
 				state,
 				baseTag.merge(this.data));
 		}
